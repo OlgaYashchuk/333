3fb494aae447aaf699e88be23ba4334bb6db4b84
OlgaYashchuk <87386462+OlgaYashchuk@users.noreply.github.com>
2022-08-29 16:40:43 +0300

    Add files via upload

---
+++ Program19.cs
@@ -0,0 +1,30 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Введите пятизначное число   ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            if (x % 100000 / 10000 == x % 10 / 1 && x % 10000 / 1000 == x % 100 / 10)
+            {
+                Console.Write("да");
+            }
+
+            else
+            {
+                Console.WriteLine("нет");
+            }
+        }
+        catch
+        {
+            Console.WriteLine("введите пятизнаячное число");
+        }
+    }
+}
---
+++ Program23.cs
@@ -0,0 +1,37 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Напишите программу, которая принимает на вход число (N) 
+        //и выдаёт таблицу кубов чисел от 1 до N.
+        //3 -> 1, 8, 27
+        //5 -> 1, 8, 27, 64, 125
+          
+
+         double Res()
+            {    
+            Console.WriteLine("Введите число N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int i=1;
+            double S= 1;
+            
+             while ( i <= N)
+             {
+                S=Math.Pow(i, 3);
+                Console.Write(S + ", ");
+                i++;
+            }
+            return S;
+
+           }   
+        try
+        {
+            Res();
+        }
+        catch
+        {
+            Console.Write("введите число"); 
+        }
+    }   
+} 
+    
